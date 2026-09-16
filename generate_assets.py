57046376cf53a8acb2dccbabc485862088241773
Lukas Misyunas <freticx@gmail.com>
2026-09-16 17:43:21 +0700

    Create generate_assets.py

---
+++ generate_assets.py
@@ -0,0 +1,42 @@
+<p align="center">
+  <picture>
+    <source media="(prefers-color-scheme: dark)" srcset="https://cdn.simpleicons.org/python/FFFFFF" />
+    <source media="(prefers-color-scheme: light)" srcset="https://cdn.simpleicons.org/python/000000" />
+    <img src="https://cdn.simpleicons.org/python/FFFFFF" width="48" height="48" alt="Python" />
+  </picture>
+  <picture>
+    <source media="(prefers-color-scheme: dark)" srcset="https://cdn.simpleicons.org/git/FFFFFF" />
+    <source media="(prefers-color-scheme: light)" srcset="https://cdn.simpleicons.org/git/000000" />
+    <img src="https://cdn.simpleicons.org/git/FFFFFF" width="48" height="48" alt="Git" />
+  </picture>
+  <picture>
+    <source media="(prefers-color-scheme: dark)" srcset="https://cdn.simpleicons.org/github/FFFFFF" />
+    <source media="(prefers-color-scheme: light)" srcset="https://cdn.simpleicons.org/github/000000" />
+    <img src="https://cdn.simpleicons.org/github/FFFFFF" width="48" height="48" alt="GitHub" />
+  </picture>
+  <picture>
+    <source media="(prefers-color-scheme: dark)" srcset="https://cdn.simpleicons.org/html5/FFFFFF" />
+    <source media="(prefers-color-scheme: light)" srcset="https://cdn.simpleicons.org/html5/000000" />
+    <img src="https://cdn.simpleicons.org/html5/FFFFFF" width="48" height="48" alt="HTML5" />
+  </picture>
+  <picture>
+    <source media="(prefers-color-scheme: dark)" srcset="https://cdn.simpleicons.org/css/FFFFFF" />
+    <source media="(prefers-color-scheme: light)" srcset="https://cdn.simpleicons.org/css/000000" />
+    <img src="https://cdn.simpleicons.org/css/FFFFFF" width="48" height="48" alt="CSS3" />
+  </picture>
+  <picture>
+    <source media="(prefers-color-scheme: dark)" srcset="https://cdn.simpleicons.org/javascript/FFFFFF" />
+    <source media="(prefers-color-scheme: light)" srcset="https://cdn.simpleicons.org/javascript/000000" />
+    <img src="https://cdn.simpleicons.org/javascript/FFFFFF" width="48" height="48" alt="JavaScript" />
+  </picture>
+  <picture>
+    <source media="(prefers-color-scheme: dark)" srcset="https://cdn.simpleicons.org/sqlite/FFFFFF" />
+    <source media="(prefers-color-scheme: light)" srcset="https://cdn.simpleicons.org/sqlite/000000" />
+    <img src="https://cdn.simpleicons.org/sqlite/FFFFFF" width="48" height="48" alt="SQLite" />
+  </picture>
+  <picture>
+    <source media="(prefers-color-scheme: dark)" srcset="https://cdn.simpleicons.org/visualstudiocode/FFFFFF" />
+    <source media="(prefers-color-scheme: light)" srcset="https://cdn.simpleicons.org/visualstudiocode/000000" />
+    <img src="https://cdn.simpleicons.org/visualstudiocode/FFFFFF" width="48" height="48" alt="VS Code" />
+  </picture>
+</p>
